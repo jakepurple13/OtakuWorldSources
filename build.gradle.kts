fb373c2c9a47c2ee1f0fba51f80d5860dc5dcd13
Jacob Rein <jakerein@gmail.com>
2023-07-25 08:49:40 -0400

    Small update

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,15 +10,11 @@ buildscript {
 }
 
 task("getSources") {
-    subprojects
-        .filter {
-
-            it.pluginManager.hasPlugin("otaku-source-application")
-            /*it.afterEvaluate {
-                it.plugins.hasPlugin("otaku-source-application")
-            }*/
-        }
-        .forEach { println(it.name) }
+    doLast {
+        subprojects
+            .filter { it.pluginManager.hasPlugin("otaku-source-application") }
+            .forEach { println("Otaku Application Source: ${it.name}") }
+    }
 }
 
 true // Needed to make the Suppress annotation work for the plugins block
