d5e3218b1fbc9a5db5622ee12f24238980f81211
Jacob Rein <jakerein@gmail.com>
2023-07-25 09:46:42 -0400

    Adding NovelUpdates

---
+++ NovelWorldSources/novelupdates/build.gradle.kts
@@ -0,0 +1,43 @@
+import plugins.SourceType
+
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    id("otaku-source-application")
+}
+
+android {
+    namespace = "com.programmersbox.novelupdates"
+    defaultConfig {
+        versionName = "1.0.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+otakuSourceInformation {
+    name = "NovelUpdates"
+    classInfo = ".NovelUpdates"
+    sourceType = SourceType.Novel
+}
+
+dependencies {
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation(libs.jsoup)
+    val ktorVersion = "2.3.2"
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("io.ktor:ktor-client-logging:$ktorVersion")
+    implementation(libs.koinAndroid)
+}
