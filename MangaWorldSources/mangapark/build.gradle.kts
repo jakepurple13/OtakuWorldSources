cf97bb6adaee70e098bbb44fb3287466baab502a
Jacob Rein <jakerein@gmail.com>
2023-07-26 07:31:26 -0400

    Adding MangaPark

---
+++ MangaWorldSources/mangapark/build.gradle.kts
@@ -0,0 +1,36 @@
+import plugins.SourceType
+
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    id("otaku-source-application")
+    kotlin("plugin.serialization")
+}
+
+android {
+    namespace = "com.programmersbox.mangapark"
+
+    defaultConfig {
+        versionName = "1.0.0"
+    }
+}
+
+otakuSourceInformation {
+    name = "MangaPark"
+    classInfo = ".MangaPark"
+    sourceType = SourceType.Manga
+}
+
+dependencies {
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation(libs.jsoup)
+    implementation(libs.bundles.okHttpLibs)
+    implementation(libs.koinAndroid)
+    implementation(projects.mangaWorldSources)
+    implementation(libs.bundles.ziplineLibs)
+    implementation(libs.kotlinxSerialization)
+}
